0fc3fbcb2e5f12c3245d745925dbc025cdc97448
Karol Kalinski <jkkalinski@gmail.com>
2020-07-19 17:12:43 -0400

    goofed around with axios, attempted to connec to Trefle API, failed

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -1,5 +1,6 @@
-const { Router } = require('express');
+const { Router, json } = require('express');
 const router = Router();
+const axios = require('axios');
 
 // eslint-disable-next-line import/no-unresolved
 const LogEntry = require('../models/LogEntry');
@@ -25,6 +26,24 @@ router.post('/', async (req, res, next) => {
       res.status(422);
     }
   }
+});
+
+
+const BASE_URL = 'https://trefle.io/api/v1/plants?token=';
+
+router.get('/', async (req, res) => {
+  try { 
+    const params = new URLSearchParams({
+    api_key: process.env.TREFLE_API_KEY,
+    feedtype: 'json',
+    ver: '1.0'
+  });
+const { data } = await axios.get(`${BASE_URL}${params}`)
+res.json(data);
+
+} catch (error) {
+   next(error);
+}
 });
 
 module.exports = router;
